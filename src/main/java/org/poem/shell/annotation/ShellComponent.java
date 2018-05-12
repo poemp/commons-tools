8e3168872b356df2f8b56dbc8011db0fe0127619
lywworking <xue_2013@sina.com>
2018-05-12 16:34:26 +0800

    ShellComponent

---
+++ src/main/java/org/poem/shell/annotation/ShellComponent.java
@@ -0,0 +1,21 @@
+package org.poem.shell.annotation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+@Service
+
+public @interface ShellComponent {
+
+    /**
+     * shell 的名字
+     * @return 名字
+     */
+    String name() default  "";
+}
