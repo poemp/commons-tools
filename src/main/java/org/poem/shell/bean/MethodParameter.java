5a30862fc892d55bafcc00055398fad41bf54557
lywworking <xue_2013@sina.com>
2018-05-12 16:33:31 +0800

    MethodParameter

---
+++ src/main/java/org/poem/shell/bean/MethodParameter.java
@@ -0,0 +1,15 @@
+package org.poem.shell.bean;
+
+
+public class MethodParameter {
+
+    private final String name;
+
+    public MethodParameter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
