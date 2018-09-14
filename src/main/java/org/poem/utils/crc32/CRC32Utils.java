6fef798d478be037974e06ebe914e9394ab4c3b4
System Administrator <root@poem.local>
2018-09-14 15:24:37 +0800

    crc32

---
+++ src/main/java/org/poem/utils/crc32/CRC32Utils.java
@@ -0,0 +1,42 @@
+package org.poem.utils.crc32;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.CRC32;
+
+public class CRC32Utils {
+
+    /**
+     * crc32
+     * @param file 文件
+     * @return
+     */
+    public static String getCRC32(File file) {
+        CRC32 crc32 = new CRC32();
+        byte[] buffer = new byte[8192];
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            int length;
+            while ((length = fileInputStream.read(buffer)) != -1) {
+                crc32.update(buffer, 0, length);
+            }
+            return crc32.getValue() + "";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (fileInputStream != null)
+                    fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
